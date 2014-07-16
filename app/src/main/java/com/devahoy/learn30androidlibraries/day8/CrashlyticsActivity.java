49c596f45db1364080e0bb606be46ba2b8128599
Chai Phonbopit <phonbopit@gmail.com>
2014-07-16 18:01:15 +0700

    remove crashlytics code :smile:

--- app/src/main/java/com/devahoy/learn30androidlibraries/day8/CrashlyticsActivity.java
+++ app/src/main/java/com/devahoy/learn30androidlibraries/day8/CrashlyticsActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 
-import com.crashlytics.android.Crashlytics;
 import com.devahoy.learn30androidlibraries.R;
 
 public class CrashlyticsActivity extends ActionBarActivity {
@@ -15,9 +14,14 @@ public class CrashlyticsActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.day8_activity_crashlytics);
 
-        Crashlytics.start(this);
+        /**
+         * Crashlytics มันต้องใช้ API_KEY จาก Plugin ของแต่ละคน generate ออกมาก่อน
+         * ถึงจะสามารถใช้งานได้ ผมเลยเอาออก และ uncomment ที่เกี่ยวกับ Crashlytics ออกก่อนนะครับ
+         * เพื่อให้ทั้งโปรเจ็คสามารถ build ผ่านครับ
+         */
+/*        Crashlytics.start(this);
 
-        Crashlytics.log("Hello initial Crashlytics");
+        Crashlytics.log("Hello initial Crashlytics");*/
 
         Button button = null;
         button.setOnClickListener(new View.OnClickListener() {
