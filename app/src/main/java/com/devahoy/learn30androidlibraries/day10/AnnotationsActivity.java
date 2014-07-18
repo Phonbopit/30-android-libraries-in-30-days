8404063f98d164be0ad793140a038499134640ab
Chai Phonbopit <phonbopit@gmail.com>
2014-07-18 23:20:01 +0700

    day10 completed :yum:

--- app/src/main/java/com/devahoy/learn30androidlibraries/day10/AnnotationsActivity.java
+++ app/src/main/java/com/devahoy/learn30androidlibraries/day10/AnnotationsActivity.java
@@ -2,16 +2,38 @@ package com.devahoy.learn30androidlibraries.day10;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.devahoy.learn30androidlibraries.R;
 
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.day10_activity_annotations)
 public class AnnotationsActivity extends ActionBarActivity {
 
+    @ViewById(R.id.greeting)
+    TextView greeting;
+
+    @ViewById
+    TextView name;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+    }
+
+    @Click(R.id.button_click)
+    void onClick() {
+        Toast.makeText(this, "Hello " ,Toast.LENGTH_LONG).show();
+    }
+
+    @AfterViews
+    void setGreeting() {
+        greeting.setText("Hello I'm John Doe");
     }
 }
