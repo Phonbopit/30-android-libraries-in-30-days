1badb52a9d85df9ee70c501b105d6b5689e0c36c
Chai Phonbopit <phonbopit@gmail.com>
2014-07-27 09:08:34 +0700

    prepare day19

---
+++ app/src/main/java/com/devahoy/learn30androidlibraries/day19/GreenDAOActivity.java
@@ -0,0 +1,12 @@
+package com.devahoy.learn30androidlibraries.day19;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class GreenDAOActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
