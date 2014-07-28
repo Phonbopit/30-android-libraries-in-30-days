42a266fa86e1804449c92838868dbba4289aa8aa
Chai Phonbopit <phonbopit@gmail.com>
2014-07-28 19:45:52 +0700

    add viewAnimationActiivty

---
+++ app/src/main/java/com/devahoy/learn30androidlibraries/day20/ViewAnimationActivity.java
@@ -0,0 +1,12 @@
+package com.devahoy.learn30androidlibraries.day20;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class ViewAnimationActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
