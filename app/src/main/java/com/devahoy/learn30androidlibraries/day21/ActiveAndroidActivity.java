9d1c1857a6f68b9701c14e494375ccfefc3408aa
Chai Phonbopit <phonbopit@gmail.com>
2014-07-29 21:44:10 +0700

    prepare day21

---
+++ app/src/main/java/com/devahoy/learn30androidlibraries/day21/ActiveAndroidActivity.java
@@ -0,0 +1,12 @@
+package com.devahoy.learn30androidlibraries.day21;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class ActiveAndroidActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
