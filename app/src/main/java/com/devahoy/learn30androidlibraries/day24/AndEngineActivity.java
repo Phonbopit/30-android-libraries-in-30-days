faa8ef79c9038ce3ace1db1d3e2ea418cbb433f3
Chai Phonbopit <phonbopit@gmail.com>
2014-08-01 21:27:49 +0700

    add andEngine and change compileSdk from 15 to 16

--- app/src/main/java/com/devahoy/learn30androidlibraries/day24/AndEngineActivity.java
+++ app/src/main/java/com/devahoy/learn30androidlibraries/day24/AndEngineActivity.java
@@ -1,4 +1,26 @@
 package com.devahoy.learn30androidlibraries.day24;
 
-public class AndEngineActivity {
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.entity.scene.Scene;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
+
+public class AndEngineActivity extends SimpleBaseGameActivity {
+
+    private static final int CAMERA_WIDTH = 800;
+    private static final int CAMERA_HEIGHT = 480;
+    
+    @Override
+    protected void onCreateResources() {
+
+    }
+
+    @Override
+    protected Scene onCreateScene() {
+        return null;
+    }
+
+    @Override
+    public EngineOptions onCreateEngineOptions() {
+        return null;
+    }
 }
