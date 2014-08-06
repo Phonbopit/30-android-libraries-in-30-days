b8191d74ad59b41a7c9b7edee8ef5a0a8f7ed6cb
Chai Phonbopit <phonbopit@gmail.com>
2014-08-07 00:02:17 +0700

    add flatui dependencies

---
+++ app/src/main/java/com/devahoy/learn30androidlibraries/day29/FlatUIActivity.java
@@ -0,0 +1,15 @@
+package com.devahoy.learn30androidlibraries.day29;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.devahoy.learn30androidlibraries.R;
+
+public class FlatUIActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.day29_activity_flatui);
+    }
+}
