6d5fc490da9c01c60a62dda53b4503742d22d6bb
Chai Phonbopit <phonbopit@gmail.com>
2014-08-08 00:38:01 +0700

    update firebase

---
+++ app/src/main/java/com/devahoy/learn30androidlibraries/day30/Chat.java
@@ -0,0 +1,39 @@
+package com.devahoy.learn30androidlibraries.day30;
+
+/**
+ * Created by phonbopit on 8/7/14.
+ */
+public class Chat {
+
+    public class KEY {
+        public static final String OWNER = "owner";
+        public static final String MESSAGE = "message";
+    }
+
+    private String message;
+    private String owner;
+
+    private Chat() {
+        // Default Constructor for firebase mapping
+    }
+    public Chat(String message, String owner) {
+        setMessage(message);
+        setOwner(owner);
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+}
--- app/src/main/java/com/devahoy/learn30androidlibraries/day30/FireBaseActivity.java
+++ app/src/main/java/com/devahoy/learn30androidlibraries/day30/FireBaseActivity.java
@@ -1,33 +1,109 @@
 package com.devahoy.learn30androidlibraries.day30;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
 
+import com.devahoy.learn30androidlibraries.R;
+import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
-import com.firebase.client.ValueEventListener;
+import com.firebase.client.Query;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 /**
  * Created by phonbopit on 8/7/14.
  */
 public class FireBaseActivity extends ActionBarActivity {
 
-    private static final String AHOY_CHAT_URL = "https://ahoychat.firebaseio.com/";
+    private static final String AHOY_CHAT_URL = "https://ahoychat.firebaseio.com";
+    private List<String> mChats = new ArrayList<String>();
+    private ListView mListView;
+    private ArrayAdapter<String> mAdapter;
+    private ImageButton mButtonSend;
+    private EditText mMessage;
+
+    private Firebase mFirebase;
+    private String mUsername;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Firebase firebase = new Firebase(AHOY_CHAT_URL).child("chat");
+        Random r = new Random();
+        mUsername = "AhoyUser" + r.nextInt(1000);
 
-        firebase.push().setValue("test");
+        setContentView(R.layout.day30_activity_firebase);
 
+        mButtonSend = (ImageButton) findViewById(R.id.button_send);
+        mMessage = (EditText) findViewById(R.id.message);
+        mListView = (ListView) findViewById(R.id.list_view);
+        mListView.setBackgroundColor(Color.parseColor("#26B895"));
 
+        mFirebase = new Firebase(AHOY_CHAT_URL).child("chat");
 
-        firebase.addValueEventListener(new ValueEventListener() {
+        queryData();
+
+        mButtonSend.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+            public void onClick(View v) {
+                sendMessage();
+            }
+        });
+    }
+
+    private void sendMessage() {
+        Chat chat = new Chat(mMessage.getText().toString(), mUsername);
+        mFirebase.push().setValue(chat);
+        mMessage.setText("");
+    }
+
+    private void queryData() {
+        Query query = mFirebase.limit(10);
+
+        query.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Map<String, Object> newPost = (Map<String, Object>) dataSnapshot.getValue();
+
+                String owner = newPost.get(Chat.KEY.OWNER).toString();
+                String message = newPost.get(Chat.KEY.MESSAGE).toString();
+
+                mChats.add(0, owner + " Say: " + message);
+
+                if (mAdapter == null) {
+
+                    mAdapter = new ArrayAdapter<String>(getApplicationContext(),
+                            android.R.layout.simple_list_item_1, mChats);
+
+                    mListView.setAdapter(mAdapter);
+                }
+                mAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                mAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
 
             }
 
